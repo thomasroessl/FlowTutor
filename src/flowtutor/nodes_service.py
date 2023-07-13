d9bd1a63e14e7b03987f68c4d0b03ed69f4abb19
Thomas Rößl <e11775192@student.tuwien.ac.at>
2023-07-13 10:41:44 +0200

    fix: Type checking error

--- src/flowtutor/nodes_service.py
+++ src/flowtutor/nodes_service.py
@@ -1,4 +1,4 @@
-from typing import Optional, Any
+from typing import Optional, Any, Type
 from os import walk, path
 import json
 from dependency_injector.wiring import Provide, inject
@@ -15,6 +15,7 @@ from flowtutor.flowchart.output import Output
 from flowtutor.flowchart.snippet import Snippet
 from flowtutor.flowchart.template import Template
 from flowtutor.util_service import UtilService
+from flowtutor.flowchart.node import Node
 
 
 class NodesService:
@@ -24,7 +25,7 @@ class NodesService:
                  utils_service: UtilService = Provide['utils_service']):
         self.utils_service = utils_service
 
-    def get_node_types(self) -> list[tuple[str, type, Optional[Any]]]:
+    def get_node_types(self) -> list[tuple[str, Type[Node], Optional[Any]]]:
         return [
             ('Assignment', Assignment, None),
             ('Call', Call, None),
@@ -38,11 +39,11 @@ class NodesService:
             ('Code Snippet', Snippet, None)
         ] + self.get_node_templates()
 
-    def get_node_templates(self) -> list[tuple[str, type, Any]]:
+    def get_node_templates(self) -> list[tuple[str, Type[Node], Any]]:
         template_file_paths: list[str] = []
         for (dirpath, _, file_names) in walk(self.utils_service.get_templates_path()):
             template_file_paths.extend([path.join(dirpath, f) for f in file_names])
-        templates: list[tuple[str, type, Any]] = []
+        templates: list[tuple[str, Type[Node], Any]] = []
         for template_file_path in template_file_paths:
             with open(template_file_path, 'r') as template_file:
                 data = json.load(template_file)
