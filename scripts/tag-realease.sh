db53bb1112bfe42dc3cb336bd93c75b71ffa8ca2
Thomas Rößl <e11775192@student.tuwien.ac.at>
2024-01-04 12:39:45 +0100

    chore: fix script

--- scripts/tag-realease.sh
+++ scripts/tag-realease.sh
@@ -1,3 +1,4 @@
 NEW_FLOWTUTOR_VERSION=$(grep -Po '^version = "\K\d+\.\d+\.\d+(?=")' pyproject.toml)
 
 git tag -a "v"$NEW_FLOWTUTOR_VERSION -m "Version "$NEW_FLOWTUTOR_VERSION | echo
+git push --atomic origin master "v"$NEW_FLOWTUTOR_VERSION
